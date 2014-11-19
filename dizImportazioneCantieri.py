33212d4144c058905bf5aeb439e604661d87388f
mamogmx <marco.carbone@gmx.com>
2014-11-19 11:15:26 +0100

    first commit importaCantieri

--- dizImportazioneCantieri.py
+++ dizImportazioneCantieri.py
@@ -3,9 +3,9 @@ keysCantieri=['id','numero','username','istruttore','data_presentazione','protoc
 
 valuesCantieri=['id','numero_pratica','owner','istruttore','data_pratica','numero_protocollo','data_protocollo','cantiere_motivazione','prescrizioni_istruttore','cantieri_nore','wf_iol','autorizzata_dal','autorizzata_al','cantieri_orario','rinnovo_sanatoria','onerosa','ricevuta','cauzione','ricevutacauzione','svincolo','importo','fisica_cognome','fisica_nome','fisica_indirizzo','fisica_comune','fisica_provincia','fisica_cap','fisica_telefono','fisica_email','fisica_comune_nato','fisica_provincia_nato','fisica_data_nato','fisica_sesso','fisica_cf','giuridica_denominazione','titolod','giuridica_indirizzo','giuridica_comune','giuridica_provincia','giuridica_cap','giuridica_piva','convenzione','data_pagamento','datainizio_eff','datafine_eff','note_utente','istruttoria_motivo_sospensione','numero_autorizzazione','data_autorizzazione']
 
-oldDict=dict()
+cantieriDict=dict()
 for i in range(len(valuesCantieri)):
-   oldDict[keysCantieri[i]]=valuesCantieri[i]
+   cantieriDict[keysCantieri[i]]=valuesCantieri[i]
 
 
 
--- importaCantieri.py
+++ importaCantieri.py
@@ -12,6 +12,13 @@ from mimetypes import MimeTypes
 import dizImportazioneCantieri
 conn_string = "postgres://postgres:postgres@192.168.1.133:5433/sitar"
 
+keysCantieri=['id','numero','username','istruttore','data_presentazione','protocollo','data_protocollo','oggetto','prescrizioni','note','stato_istanze','datainizio','datafine','orario','rinnovoinsanatoria','onerosa','ricevuta','cauzione','ricevutacauzione','svincolo','importo','cognome','nome','indirizzo','comune','prov','cap','telefono','email','comunato','provnato','datanato','sesso','codfis','ragsoc','titolod','sede','comuned','provd','capd','piva','convenzione','data_pagamento','datainizio_eff','datafine_eff','note_utente','motivo_sospensione','n_autorizzazione','data_autorizzazione']
+valuesCantieri=['id','numero_pratica','owner','istruttore','data_pratica','numero_protocollo','data_protocollo','cantiere_motivazione','prescrizioni_istruttore','cantieri_nore','wf_iol','autorizzata_dal','autorizzata_al','cantieri_orario','rinnovo_sanatoria','onerosa','ricevuta','cauzione','ricevutacauzione','svincolo','importo','fisica_cognome','fisica_nome','fisica_indirizzo','fisica_comune','fisica_provincia','fisica_cap','fisica_telefono','fisica_email','fisica_comune_nato','fisica_provincia_nato','fisica_data_nato','fisica_sesso','fisica_cf','giuridica_denominazione','titolod','giuridica_indirizzo','giuridica_comune','giuridica_provincia','giuridica_cap','giuridica_piva','convenzione','data_pagamento','datainizio_eff','datafine_eff','note_utente','istruttoria_motivo_sospensione','numero_autorizzazione','data_autorizzazione']
+
+cantieriDict=dict()
+for i in range(len(valuesCantieri)):
+   cantieriDict[keysCantieri[i]]=valuesCantieri[i]
+
 def findData(app):
     psite = app.unrestrictedTraverse("istanze")
     engine = sql.create_engine(conn_string)
